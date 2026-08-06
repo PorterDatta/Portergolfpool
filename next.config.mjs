4c459b78221753b85413cfa147030b4f7075d9f2
PorterDatta <pdatta4@vols.utk.edu>
2026-08-06 14:35:24 -0400

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,7 @@
 const nextConfig = {
   images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
   experimental: { serverComponentsExternalPackages: ['pdfkit', 'exceljs'] },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
 export default nextConfig;
